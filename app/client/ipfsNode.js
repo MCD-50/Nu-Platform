a72ea60ab23612a1107212a000a2aaf9efe89aab
MCD-50 <ayush.as.shukla@gmail.com>
2019-04-06 20:02:41 +0530

    Infura fixes

--- app/client/ipfsNode.js
+++ app/client/ipfsNode.js
@@ -1,5 +1,5 @@
-const IPFS = require("ipfs");
-const node = new IPFS({ host: "localhost", port: 5001, protocol: "http" });
+const IPFS = require("ipfs-api");
+const node = new IPFS({ host: "ipfs.infura.io", port: 5001, protocol: "https" });
 //const node = new IPFS({ host: "localhost", port: 5001, protocol: "http" });
 
 let nodeFactory = null;
